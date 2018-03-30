995de7199800d6e4306878a3277efff85317dd6e
Daryl Concha <concha.daryl@gmail.com>
2018-03-30 14:14:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ function increaseRankBy(n){
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
 
   for (let i = 0; i < lis.length; i++){
-    let currNum = lis[i].innerHTML;
-    lis[i].innerHTML = (currNum + n).toString();
+    lis[i].innerHTML = parseInt((lis[i].innerHTML + n).toString());
   }
 }
 
